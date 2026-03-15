167a4e7376cf5a1a3f9fb8b8f2b922f44d0db6fc
Rjhubri <jhubri57956@gmail.com>
2026-03-15 07:36:43 -0700

    Refactor Flask routes for dynamic page rendering
    Refactored route definitions to use a single pages function with dynamic URL parameters. Updated the app to run on host 0.0.0.0 and port 10000.

--- app.py
+++ app.py
@@ -2,49 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route('/page1')
-def page1():
-    return render_template('page1.html')
+@app.route("/page<int:num>")
+def pages(num):
+    return render_template(f"page{num}.html")
 
-@app.route('/page2')
-def page2():
-    return render_template('page2.html')
-
-@app.route('/page3')
-def page3():
-    return render_template('page3.html')
-
-@app.route('/page4')
-def page4():
-    return render_template('page4.html')
-
-@app.route('/page5')
-def page5():
-    return render_template('page5.html')
-
-@app.route('/page6')
-def page6():
-    return render_template('page6.html')
-
-@app.route('/page7')
-def page7():
-    return render_template('page7.html')
-
-@app.route('/page8')
-def page8():
-    return render_template('page8.html')
-
-@app.route('/page9')
-def page9():
-    return render_template('page9.html')
-
-@app.route('/page10')
-def page10():
-    return render_template('page10.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
